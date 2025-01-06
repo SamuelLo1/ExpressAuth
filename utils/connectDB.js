4e9694f1a70ea9c6fc57fcae5a270c60add7189e
SamuelLo <samuello300@gmail.com>
2025-01-05 17:06:00 -0800

    fixing warning message with deprecated methods

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -7,7 +7,7 @@ async function connectDB() {
         //
         const secrets = await getSecrets();
         const MONGO_URI = JSON.parse(secrets.SecretString).MONGO_URI;
-        await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(MONGO_URI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
